5bb53ed6f37b1e76820bcbed6fdc4199ee146779
Jason Kulatunga <jk17@ualberta.ca>
2013-11-30 22:02:31 -0800

    update entrypoint with messages and start bash.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
+echo "loading env.sh file"
 source /var/docker/env.sh
 
 ########################################################################################################################
 # Configure and Run Couchpotato
+echo "configuring Couchpotato"
 cheetah f --env --oext conf $COUCHPOTATO_HOME/settings
 # Start the Couchpotato application
 #python /var/docker/couchpotato/CouchPotato.py
 
 ########################################################################################################################
 # Configure and Run Sickbeard
+echo "configuring sickbeard"
 cheetah f --env --oext ini $SICKBEARD_HOME/config
 # Start the Sickbeard application
 #python /var/docker/sickbeard/SickBeard.py
 
 ########################################################################################################################
 # Configure and Run Deluge
-
+echo "configuring deluge"
 # Generate the Deluge Configuration files from ENV variables.
 cheetah f --env --oext conf $DELUGE_CONFIG_HOME/core
 cheetah f --env --oext conf $DELUGE_CONFIG_HOME/web
@@ -24,4 +27,6 @@ cheetah f --env --oext conf $DELUGE_CONFIG_HOME/auth
 mv $DELUGE_CONFIG_HOME/auth.conf $DELUGE_CONFIG_HOME/auth
 
 # Start the Deluge Daemon
-deluged -d
+#deluged -d
+
+/bin/bash
